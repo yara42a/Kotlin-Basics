163d3959a42a5403a14b8cc45592c68fabed33e6
yara42a <106034563+yara42a@users.noreply.github.com>
2023-01-01 18:54:32 +0300

    Add files via upload

---
+++ Ex1.kt
@@ -0,0 +1,12 @@
+package com.example.ex1
+
+fun main(){
+    val s:String="Abdullah"
+    val f:Float=20.6F
+    val d:Double=8.9746325
+    val by:Byte=25
+    val sh:Short=2022
+    val l:Long=321984526954
+    val b:Boolean=true
+    val c:Char='a'
+}
